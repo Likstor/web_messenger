2f2a6a16b2f90fe971d8dfc0f58c568d067cbcc5
Likstor <94634271+Likstor@users.noreply.github.com>
2023-09-29 17:47:13 +0300

    removed .venv and started using pipenv

--- web_messenger/web_messenger/settings.py
+++ web_messenger/web_messenger/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z@_*hj6g(x5u5+b!dzmfd2%=fobm8viqu&z!9z=)vuy7udn@#2'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -77,11 +78,11 @@ WSGI_APPLICATION = 'web_messenger.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv('PGNAME'),
-        'USER': os.getenv('PGUSER'),
-        'PASSWORD': os.getenv('PGPASS'),
-        'HOST': os.getenv('PGHOST'),
-        'PORT': os.getenv('PGPORT'),
+        "NAME": config('PGNAME'),
+        'USER': config('PGUSER'),
+        'PASSWORD': config('PGPASS'),
+        'HOST': config('PGHOST'),
+        'PORT': config('PGPORT'),
         },
 }
 
