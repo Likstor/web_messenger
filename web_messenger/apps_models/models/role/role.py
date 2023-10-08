13dd45f0504a4a6365877351b429a9a1597400dd
phenibutbarriers <124178549+phenibutbarriers@users.noreply.github.com>
2023-10-08 17:37:12 +0300

    6AJIBAHKA v2 complete

--- web_messenger/apps_models/models/role/role.py
+++ web_messenger/apps_models/models/role/role.py
@@ -1,4 +1,13 @@
 from django.db import models
+from ..server.server import Server
+from django.utils.translation import gettext_lazy as _
+from ..rights.role_rights import RoleRights
 
 class Role(models.Model):
-    ...
+    title = models.CharField(_('role title'), max_length=20)
+    server = models.ForeignKey(Server, on_delete=models.CASCADE)
+    color = models.CharField()
+    role_rights = models.OneToOneField(
+        RoleRights,
+        on_delete=models.CASCADE
+    )
