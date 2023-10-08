8c9f8dbe8188c06b0b90cbf9098256d464af8f9e
phenibutbarriers <124178549+phenibutbarriers@users.noreply.github.com>
2023-10-08 17:15:30 +0300

    6AJIBAHKA created

--- web_messenger/apps_models/models/server/server.py
+++ web_messenger/apps_models/models/server/server.py
@@ -1,4 +1,7 @@
 from django.db import models
+from ..user.user import User
+from django.utils.translation import gettext_lazy as _
 
 class Server(models.Model):
-    ...
+    title = models.CharField(_('server title'))
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
