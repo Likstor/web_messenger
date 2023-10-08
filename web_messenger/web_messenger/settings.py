5e9eae9374c5a1970d6bb37fb34b2b6f285eb5d1
Likstor <94634271+Likstor@users.noreply.github.com>
2023-10-08 16:09:47 +0300

    models init

--- web_messenger/web_messenger/settings.py
+++ web_messenger/web_messenger/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps_models.apps.AppsModelsConfig',
 ]
 
 MIDDLEWARE = [
