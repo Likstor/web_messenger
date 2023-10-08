3baf2086f759656bd83ed57eaa867e68d73dc4aa
Likstor <94634271+Likstor@users.noreply.github.com>
2023-10-08 19:56:30 +0300

    .gitignore update and message fix

--- web_messenger/app_models/models/message/message.py
+++ web_messenger/app_models/models/message/message.py
@@ -2,9 +2,11 @@ from django.db import models
 from ..channel.text_channel import TextChannel
 from ..user.user import User
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
 class Message(models.Model):
-    text_channel = models.ForeignKey(TextChannel, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.TextField(_('text in message'))
+    text_channel = models.ForeignKey(TextChannel, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    text = models.TextField(_('text in message'), null=True)
     file = models.FileField(_('file that will send'), upload_to='files/user_files', null=True)
+    time = models.DateTimeField(_('message time'), default=timezone.now)
