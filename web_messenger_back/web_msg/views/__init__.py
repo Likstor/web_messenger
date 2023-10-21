2feeb96744dc78994d0b82cf38867a066d006fff
brek076 <38405777+brek076@users.noreply.github.com>
2023-10-21 23:36:26 +0300

    Update __init__.py
    Добавление инитов

--- web_messenger_back/web_msg/views/__init__.py
+++ web_messenger_back/web_msg/views/__init__.py
@@ -1,2 +1,6 @@
 from .login_view import user_login
+from .server_page import server_home
+from .server_page import ServerDetailView
+from .text_channel import TextChannelDetailView
+from .text_channel import channel_chat
 from .index import index
