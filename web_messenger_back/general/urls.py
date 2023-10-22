520f3e58deaaf2675c0983d64f8a02590e39f338
brek076 <38405777+brek076@users.noreply.github.com>
2023-10-22 11:55:25 +0300

    Rough file transfer
    Transferring files to the current version of the branch

--- web_messenger_back/general/urls.py
+++ web_messenger_back/general/urls.py
@@ -20,8 +20,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import include
 from django.views.generic import RedirectView
+from web_msg.views import index
 
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
     path('', include('web_msg.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
--- web_messenger_back/web_msg/urls.py
+++ web_messenger_back/web_msg/urls.py
@@ -52,5 +52,7 @@ urlpatterns = [
              template_name='web_msg/account/password_reset/password_reset_complete.html'
              ), 
          name='password_reset_complete', ),
-
+    path('', views.index, name='index'),
+    path('server/<int:pk>/', views.ServerDetailView.as_view(), name='server-detail'),
+    path('channel/<int:pk>/', views.ChannelTextDetailView.as_view(), name='channel-detail')
 ]
