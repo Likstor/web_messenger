9655fc9e7abf8ddeb1e82d282b7c8c535d83afd0
Likstor <94634271+Likstor@users.noreply.github.com>
2023-11-01 21:55:28 +0300

    sign in & sign up complete

--- web_messenger_back/web_msg/views/account/sign_up.py
+++ web_messenger_back/web_msg/views/account/sign_up.py
@@ -1,15 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from ...forms import SignUpForm
 
 def user_sign_up(request):
-    if request.method == 'POST':
-        sign_up_form = SignUpForm(request.POST)
-        if sign_up_form.is_valid():
-            create_user = sign_up_form.save(commit=False)
-            create_user.set_password(sign_up_form.cleaned_data['password_1'])
-            create_user.save()
-            return render(request, 'web_msg/account/sign_up/sign_up_done.html', {'create_user': create_user})
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/home/')
     else:
-        sign_up_form = SignUpForm()
-    return render(request, 'web_msg/account/sign_up/sign_up.html', {'sign_up_form': sign_up_form})
+        if request.method == 'POST':
+            sign_up_form = SignUpForm(request.POST)
+            if sign_up_form.is_valid():
+                create_user = sign_up_form.save(commit=False)
+                create_user.set_password(sign_up_form.cleaned_data['password_1'])
+                create_user.save()
+                return render(request, 'web_msg/account/sign_up/sign_up_done.html', {'create_user': create_user})
+        else:
+            sign_up_form = SignUpForm()
+        return render(request, 'web_msg/account/sign_up/sign_up.html', {'sign_up_form': sign_up_form})
