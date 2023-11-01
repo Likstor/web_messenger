7a2b931571e91d5c29f947e5bfb879247d090dde
phenibutbarriers <124178549+phenibutbarriers@users.noreply.github.com>
2023-11-01 18:01:37 +0300

    make home page

--- web_messenger_back/web_msg/views/home.py
+++ web_messenger_back/web_msg/views/home.py
@@ -1,7 +1,18 @@
+from typing import Any
 from django.contrib.auth.decorators import login_required
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.views.generic import ListView
+from app_models.models import *
 
 
-@login_required
-def home(request):
-    return render(request, 'web_msg/home.html', {'section': 'home'})
+class Home(ListView):
+    model = Server
+    template_name='web_msg/home.html'
+    context_object_name = 'my_servers_list'
+
+    def get_queryset(self):
+        return Server.objects.all()
+
+
+
