75abd2512bcd202d407c935be6cf80168edad04d
phenibutbarriers <124178549+phenibutbarriers@users.noreply.github.com>
2023-11-01 18:40:18 +0300

    login + home

--- web_messenger_back/web_msg/views/home.py
+++ web_messenger_back/web_msg/views/home.py
@@ -4,15 +4,12 @@ from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import ListView
 from app_models.models import *
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class Home(ListView):
+class Home(LoginRequiredMixin, ListView):
     model = Server
     template_name='web_msg/home.html'
     context_object_name = 'my_servers_list'
-
-    def get_queryset(self):
-        return Server.objects.all()
-
 
 
