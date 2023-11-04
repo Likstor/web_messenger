3774e489ca26659cda641f912fd287009be25412
brek076 <38405777+brek076@users.noreply.github.com>
2023-11-04 15:15:05 +0300

    TEST

--- web_messenger_back/web_msg/urls.py
+++ web_messenger_back/web_msg/urls.py
@@ -58,3 +58,4 @@ urlpatterns = [
     # path('server/<int:pk>/channel/<int:pk>/', views.ChannelTextDetailView.as_view(), name='channel-detail'),
     path('channel/<int:pk>/', views.ChannelTextDetailView.as_view(), name='channel-detail')
 ]
+pass
