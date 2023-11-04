456eb725aa8b2cfecc199dfabe12a7171cdd648f
brek076 <38405777+brek076@users.noreply.github.com>
2023-11-04 15:17:56 +0300

    Revert "TEST"
    This reverts commit 3774e489ca26659cda641f912fd287009be25412.

--- web_messenger_back/web_msg/urls.py
+++ web_messenger_back/web_msg/urls.py
@@ -57,5 +57,4 @@ urlpatterns = [
     path('server/<int:pk>/', views.ServerDetailView.as_view(), name='server-detail'),
     # path('server/<int:pk>/channel/<int:pk>/', views.ChannelTextDetailView.as_view(), name='channel-detail'),
     path('channel/<int:pk>/', views.ChannelTextDetailView.as_view(), name='channel-detail')
-<<<<<<< HEAD
 ]
