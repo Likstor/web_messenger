e4924d4632b5c82d1bd39560a0a86bb0b67abfa4
Likstor <94634271+Likstor@users.noreply.github.com>
2023-11-07 19:59:11 +0300

    rest add

--- web_messenger_back/general/urls.py
+++ web_messenger_back/general/urls.py
@@ -24,4 +24,5 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('web_msg.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
