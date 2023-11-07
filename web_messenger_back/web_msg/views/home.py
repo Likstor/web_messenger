6077bea9c18ebf94f43ef2ae6975e95aa0c4d2cc
brek076 <38405777+brek076@users.noreply.github.com>
2023-11-07 20:01:12 +0300

    Update page

--- web_messenger_back/web_msg/views/home.py
+++ web_messenger_back/web_msg/views/home.py
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-from typing import Any
-from django.contrib.auth.decorators import login_required
-from django.db.models.query import QuerySet
-from django.shortcuts import render
-from django.views.generic import ListView
-from app_models.models import *
-
-
-class Home(ListView):
-    model = Server
-    template_name='web_msg/home.html'
-    context_object_name = 'my_servers_list'
-
-    def get_queryset(self):
-        return Server.objects.all()
-
-
-
-=======
-
 from django.shortcuts import render
 from django.views.generic import ListView
 from app_models.models import *
@@ -33,4 +12,3 @@ class Home(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return (Server.objects.all())
->>>>>>> ed101c25e887c8c682a6a22dbe681121e0b9767b
