a543a92ff85740f0a787b7247a9c46eeddd1797e
brek076 <egor222777@gmail.com>
2023-12-05 21:24:50 +0300

    Deleted the excess and done style chats
    Удалил лишнее и доделал визуал чатов

--- web_messenger_back/general/settings.py
+++ web_messenger_back/general/settings.py
@@ -52,7 +52,7 @@ AUTH_USER_MODEL = 'app_models.User'
 # Application definition
 
 INSTALLED_APPS = [
-    "daphne",
+    "daphne",   
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- web_messenger_back/web_msg/consumers.py
+++ web_messenger_back/web_msg/consumers.py
@@ -7,7 +7,6 @@ from asgiref.sync import sync_to_async
 import datetime
 import json
 
-
 class ChatConsumer(AsyncWebsocketConsumer):
 
 	async def connect(self):
